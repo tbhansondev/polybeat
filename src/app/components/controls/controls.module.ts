8fe2e2e2781574e00412ccaaa9204415f5e68a6e
Tom <Hanson>
2022-07-03 18:11:50 +0100

    put mat-cards around the controls

--- src/app/components/controls/controls.module.ts
+++ src/app/components/controls/controls.module.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { NumberSpinnerModule } from '../number-spinner/number-spinner.module';
 import { TrackSuppressBtnModule } from '../track-suppress-btn/track-suppress-btn.module';
 import { ControlsComponent } from './controls.component';
 
 @NgModule({
-  imports: [CommonModule, NumberSpinnerModule, TrackSuppressBtnModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatDividerModule,
+    NumberSpinnerModule,
+    TrackSuppressBtnModule,
+  ],
   declarations: [ControlsComponent],
   exports: [ControlsComponent],
 })
--- src/app/components/number-spinner/number-spinner.module.ts
+++ src/app/components/number-spinner/number-spinner.module.ts
@@ -2,11 +2,18 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { NumberSpinnerComponent } from './number-spinner.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+  ],
   declarations: [NumberSpinnerComponent],
   exports: [NumberSpinnerComponent],
 })
